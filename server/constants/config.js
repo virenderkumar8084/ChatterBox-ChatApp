8bc566d26c2e336771977d22d044708fe1b0ad09
Virender Kumar <111553977+virenderkumar8084@users.noreply.github.com>
2024-10-20 02:18:29 +0530

    Add files via upload

---
+++ server/constants/config.js
@@ -0,0 +1,8 @@
+const corsOptions = {
+    origin: ["http://localhost:5173", "http://localhost:4173", process.env.CLIENT_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+};
+const CHATTERBOX_TOKEN = "chatterbox-token";
+
+export { corsOptions, CHATTERBOX_TOKEN };
